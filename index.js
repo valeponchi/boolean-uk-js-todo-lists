e68763aa8bf88c1aad4258a7ead1ee777e5d9ab9
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-04-29 21:46:26 +0100

    completed (excluding challenges)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Repo: boolean-uk-js-todo-lists
+
 /* 
 The users and todos variables have all the data for you to work with
 Check your console to see the result of the following console.logs, 
@@ -5,20 +7,81 @@ and inspect the data
 */
 
 console.log("users: ", users);
+// {
+//   id: 1,
+//   name: "Leanne Graham",
+//   username: "Bret",
+//   email: "Sincere@april.biz",
+//   address: {
+//     street: "Kulas Light",
+//     suite: "Apt. 556",
+//     city: "Gwenborough",
+//     zipcode: "92998-3874",
+//     geo: {
+//       lat: "-37.3159",
+//       lng: "81.1496"
+//     }
+//   },
+//   phone: "1-770-736-8031 x56442",
+//   website: "hildegard.org",
+//   company: {
+//     name: "Romaguera-Crona",
+//     catchPhrase: "Multi-layered client-server neural-net",
+//     bs: "harness real-time e-markets"
+//   }
+// },
 
 console.log("todos: ", todos);
 
-// Repo: boolean-uk-js-todo-lists
-
-// Description
-// This is the first try at a common program built by new developers, the todo list. You'll have to use everything we've covered so far in JS to tackle this challenge
-
-// Instructions
-// - Use this starting template, you'll find two objects with data for you to use => https://codesandbox.io/s/todo-lists-tr122?from-embed=&file=/index.js
-// - Do not code your solution in CodeSandbox
+//1.
 // - Create an alert that lists all users, with their ids, names and what city they're from
+let usersData = []
+
+for (const user of users) {
+  usersData.push([user.id, user.name, user.address.city])
+  // [
+  //   [usersData += user.id + " "],  
+  //   [usersData += user.name + ", from "],
+  //   [usersData += user.address.city + " \n"] // \n vai a capo
+  // ]
+}
+// alert(usersData)
+console.log(usersData)
+
+
+
+
+// 2.
 // - Prompt the user for a user id
+let yourID = Number(prompt("Please type your ID"))
+
 // - Display an alert with the username and all the todos titles that belong to that user 
+
+// {
+//   userId: 1,
+//   id: 1,
+//   title: "delectus aut autem",
+//   completed: false
+// },
+
+
+let yourList = []
+for (const user of users) {
+  if (yourID === user.id)  {
+    yourList.push(user.username)
+  } 
+}
+
+let titles = []
+for (const todo of todos) {
+  if (yourID === todo.userId) {
+    titles.push(todo.title)
+  }
+}
+yourList.push(titles)
+console.log(yourList)
+
+
 
 // Tips
 // - The user id creates an identifiable relationship with the todos
